a5ee7f5ed08846e4c5f961d23dfa97d7ab7a8d01
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-17 12:55:02 +0100

    Implemented the code for the Gallery.cs file, haven't tested it yet, so it will probably not work at all. Working with streams and such is odd

--- Galleriet/Galleriet/Gallery.cs
+++ Galleriet/Galleriet/Gallery.cs
@@ -10,29 +10,57 @@ namespace Galleriet
 {
     public class Gallery
     {
-        Regex ApprovedExtensions;
-        string PhysicalUploadedImagesPath;
-        Regex SanitizePath;
+        static readonly Regex ApprovedExtensions;
+        static string PhysicalUploadedImagesPath;
+        static readonly Regex SanitizePath;
 
-        Gallery()
+        static Gallery()
         {
-
+            ApprovedExtensions = new Regex(string.Format("{0}", "^.*\\.(gif|jpg|png)$"));
+            var invalidChars = new string(Path.GetInvalidFileNameChars());
+            SanitizePath = new Regex(string.Format("[{0}]", Regex.Escape(invalidChars)));
+            PhysicalUploadedImagesPath = Path.Combine(AppDomain.CurrentDomain.GetData("APPBASE").ToString(), "Images");
         }
         IEnumerable<string> GetImageNames()
         {
-            return new List<string>();
+            DirectoryInfo df = new DirectoryInfo(PhysicalUploadedImagesPath);
+            List<string> files = new List<string>();
+            foreach(FileInfo fi in df.GetFiles())
+            {
+                files.Add(fi.ToString());
+            }
+            return files;
         }
-        bool ImageExsists(string name)
+        static bool ImageExsists(string name)
         {
-            return true;
+            return File.Exists(Path.Combine(PhysicalUploadedImagesPath, name));
         }
         bool IsValidImage(Image image)
         {
-            return true;
+            return image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Gif.Guid;
         }
         string SaveImage(Stream stream, string fileName)
         {
-            return "yes";
+            string thumbnailPath = Path.Combine(PhysicalUploadedImagesPath, "thumbnails");
+            var image = System.Drawing.Image.FromStream(stream);
+            if (!IsValidImage(image))
+                throw new ArgumentException();
+
+            var thumbnail = image.GetThumbnailImage(60, 45, null, System.IntPtr.Zero);
+            int counter = 0;
+            string tempFileName = fileName;
+
+            while (File.Exists(Path.Combine(thumbnailPath, fileName)))
+            {
+                tempFileName = fileName + counter;
+            }
+            thumbnail.Save(Path.Combine(thumbnailPath, tempFileName));
+
+            using(var fs = File.Create(Path.Combine(PhysicalUploadedImagesPath, tempFileName)))
+            {
+                stream.CopyTo(fs);
+            }
+            return Path.Combine(PhysicalUploadedImagesPath, tempFileName);
         }
     }
 }
