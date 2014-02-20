1d65c79454d117e492fa4e44270f0775c19af403
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-20 16:24:27 +0100

    The current image is marked with a green border around it

--- Galleriet/Galleriet/Scripts/Main.js
+++ Galleriet/Galleriet/Scripts/Main.js
@@ -1,5 +1,29 @@
-﻿$(function () {
+﻿/*$(function () {
+    "use strict";
+
+    console.log(location);
     var anchor = $('#imageswitcher a');
 
-    console.log(anchor);
-})
+    anchor.each(function (index) {
+        console.log($(this));
+        var href = $(this).attr('href');
+        if (href === decodeURIComponent(location.search).split('&').filter(function (s) { return s.indexOf('file=') !== -1 }).sort()[0])
+            $(this).parent.classList.add('current');
+    });
+})*/
+$(function () {
+    'use strict';
+
+    var anchor = $('#imageswitcher a');
+
+    anchor.each(function (index) {
+        var href = $(this).attr('href');
+
+        console.log($(this));
+
+        var list = decodeURIComponent(location.search).split('&').filter(function (s) { return s.indexOf('file=') !== -1 }).sort()[0];
+        if (href === list) {
+            $(this).addClass('current');
+        }
+    });
+});
