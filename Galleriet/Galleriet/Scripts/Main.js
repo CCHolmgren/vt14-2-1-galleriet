841fb870c4f2ced0f05b1616949333b2fd52d6df
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2014-02-25 11:34:36 +0100

    Somce comments in the javascript and cleaning it up a bit

--- Galleriet/Galleriet/Scripts/Main.js
+++ Galleriet/Galleriet/Scripts/Main.js
@@ -1,34 +1,32 @@
-﻿/*$(function () {
-    "use strict";
-
-    console.log(location);
-    var anchor = $('#imageswitcher a');
-
-    anchor.each(function (index) {
-        console.log($(this));
-        var href = $(this).attr('href');
-        if (href === decodeURIComponent(location.search).split('&').filter(function (s) { return s.indexOf('file=') !== -1 }).sort()[0])
-            $(this).parent.classList.add('current');
-    });
-})*/
-$(function () {
+﻿$(function () {
     'use strict';
 
+    //Remove the success message
     $('#successbutton').click(function () {
         $(this).parent().remove();
     });
 
     var anchor = $('#imageswitcher a');
+
+    //Count the amount of images so we get the offset for the scrollLeft
     var images = 0;
-    anchor.each(function (index) {
+    anchor.each(function () {
         var href = $(this).attr('href');
         images += 1;
-        //console.log($(this));
 
-        var list = decodeURIComponent(location.search).split('&').filter(function (s) { return s.indexOf('file=') !== -1 }).sort()[0];
+        //Ugly solution, but it does what it should
+        //decodeURIComponent(location.search) gives us the decoded queryString
+        //split the string a & if there are more values in it
+        //filter the string based on if there is a file= in it
+        //since there can be several of them we use the first one from the sorted result
+        var listA = decodeURIComponent(location.search).split('&'),
+            list = listA.filter(function (s) { return s.indexOf('file=') !== -1 }).sort()[0];
         if (href === list) {
-            $(this).addClass('current');
-            console.log(".liimageswitcher.scrollLeft ", $('.liimageswitcher').animate({ scrollLeft: images * 45 }, "slow"));
+            $(this).addClass('currentimage');
+            //console.log the result, which isn't really necessary
+            //Since animate() isnt sideeffect free we can do this and still get a result
+            //we use animate since it doesn't just jump, it scrolls instead
+            console.log(".liimageswitcher.scrollLeft ", $('.liimageswitcher').animate({ scrollLeft: (images-1) * 30 }, "slow"));
         }
     });
 });
